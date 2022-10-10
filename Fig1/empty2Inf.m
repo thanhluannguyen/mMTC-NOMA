17ba56caf213b3e78e156e3ffd7200fe5e7bc3de
Thanh Luan Nguyen <75961850+thanhluannguyen@users.noreply.github.com>
2022-10-10 09:04:08 -0400

    Delete Fig1 directory

--- Fig1/empty2Inf.m
+++
@@ -1,3 +0,0 @@
-function out = empty2Inf(X)
-    out = max([ min([X,Inf]),X ]); % - Convert "X = []" to "X = Inf" while Keeping "X >< 0";
-end
--- Fig1/empty2zero.m
+++
@@ -1,3 +0,0 @@
-function out = empty2zero(X)
-    out = min([ max([X,0]),X ]); % - Convert "X = []" to "X = 0" while Keeping "X >< 0";
-end
